47215aadbee56c61ab3b51542a1d64dee2b6e1e5
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-11 22:16:27 -0800

    Add basic run class

---
+++ wc-stats-tracker/Models/WCRun.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wc_stats_tracker.Models
+{
+    /// <summary>
+    /// Data for a WC Run
+    /// </summary>
+    public class WCRun
+    {
+        /// <summary>
+        /// Length of the Run
+        /// </summary>
+        private TimeSpan RunLength { get; set; }
+        /// <summary>
+        /// Number of characters found during the run
+        /// </summary>
+        private int CharactersFound { get; set; }
+        /// <summary>
+        /// Number of espers found during the run
+        /// </summary>
+        private int EspersFound { get; set; }
+        /// <summary>
+        /// Number of dragons killed during the run
+        /// </summary>
+        private int DragonsKilled { get; set; }
+        /// <summary>
+        /// Number of bosses killed during the run
+        /// </summary>
+        private int BossesKilled { get; set; }
+        /// <summary>
+        /// Number of checks done during the run
+        /// </summary>
+        private int ChecksDone { get; set; }
+        /// <summary>
+        /// Number of chests opened during the run
+        /// </summary>
+        private int ChestsOpened { get; set; }
+    }
+}
