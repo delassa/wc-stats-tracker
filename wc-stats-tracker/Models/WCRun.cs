994503c30fde7cc08c5bcd285ba480c2c8d2f6b2
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-12 10:24:34 -0800

    Fix properties

--- wc-stats-tracker/Models/WCRun.cs
+++ wc-stats-tracker/Models/WCRun.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace wc_stats_tracker.Models
+namespace WCStatsTracker.Models
 {
     /// <summary>
     /// Data for a WC Run
@@ -14,34 +15,34 @@ namespace wc_stats_tracker.Models
         /// <summary>
         /// Length of the Run
         /// </summary>
-        private TimeSpan RunLength { get; set; }
+        public TimeSpan RunLength { get; set; }
         /// <summary>
         /// Number of characters found during the run
         /// </summary>
-        private int CharactersFound { get; set; }
+        public int CharactersFound { get; set; }
         /// <summary>
         /// Number of espers found during the run
         /// </summary>
-        private int EspersFound { get; set; }
+        public int EspersFound { get; set; }
         /// <summary>
         /// Number of dragons killed during the run
         /// </summary>
-        private int DragonsKilled { get; set; }
+        public int DragonsKilled { get; set; }
         /// <summary>
         /// Number of bosses killed during the run
         /// </summary>
-        private int BossesKilled { get; set; }
+        public int BossesKilled { get; set; }
         /// <summary>
         /// Number of checks done during the run
         /// </summary>
-        private int ChecksDone { get; set; }
+        public int ChecksDone { get; set; }
         /// <summary>
         /// Number of chests opened during the run
         /// </summary>
-        private int ChestsOpened { get; set; }
+        public int ChestsOpened { get; set; }
         /// <summary>
-        /// Did this run have a kt skip
-        /// </summary>
-        private bool DidKTSkip { get; set; }
+              /// Did this run have a kt skip
+              /// </summary>
+        public bool DidKTSkip { get; set; }
     }
 }
