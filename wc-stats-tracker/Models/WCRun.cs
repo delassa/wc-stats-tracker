8f317307dbd0a5b296304c170e82160b6c679f7c
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-15 18:57:48 -0800

    Update with ID

--- wc-stats-tracker/Models/WCRun.cs
+++ wc-stats-tracker/Models/WCRun.cs
@@ -5,44 +5,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WCStatsTracker.Models
+namespace WCStatsTracker.Models;
+
+/// <summary>
+/// Data for a WC Run
+/// </summary>
+public class WCRun
 {
     /// <summary>
-    /// Data for a WC Run
+    /// Unique identifier of the run
     /// </summary>
-    public class WCRun
-    {
-        /// <summary>
-        /// Length of the Run
-        /// </summary>
-        public TimeSpan RunLength { get; set; }
-        /// <summary>
-        /// Number of characters found during the run
-        /// </summary>
-        public int CharactersFound { get; set; }
-        /// <summary>
-        /// Number of espers found during the run
-        /// </summary>
-        public int EspersFound { get; set; }
-        /// <summary>
-        /// Number of dragons killed during the run
-        /// </summary>
-        public int DragonsKilled { get; set; }
-        /// <summary>
-        /// Number of bosses killed during the run
-        /// </summary>
-        public int BossesKilled { get; set; }
-        /// <summary>
-        /// Number of checks done during the run
-        /// </summary>
-        public int ChecksDone { get; set; }
-        /// <summary>
-        /// Number of chests opened during the run
-        /// </summary>
-        public int ChestsOpened { get; set; }
-        /// <summary>
-              /// Did this run have a kt skip
-              /// </summary>
-        public bool DidKTSkip { get; set; }
-    }
+    public int Id { get; set; }
+    /// <summary>
+    /// Length of the Run
+    /// </summary>
+    public TimeSpan RunLength { get; set; }
+    /// <summary>
+    /// Number of characters found during the run
+    /// </summary>
+    public int CharactersFound { get; set; }
+    /// <summary>
+    /// Number of espers found during the run
+    /// </summary>
+    public int EspersFound { get; set; }
+    /// <summary>
+    /// Number of dragons killed during the run
+    /// </summary>
+    public int DragonsKilled { get; set; }
+    /// <summary>
+    /// Number of bosses killed during the run
+    /// </summary>
+    public int BossesKilled { get; set; }
+    /// <summary>
+    /// Number of checks done during the run
+    /// </summary>
+    public int ChecksDone { get; set; }
+    /// <summary>
+    /// Number of chests opened during the run
+    /// </summary>
+    public int ChestsOpened { get; set; }
+    /// <summary>
+          /// Did this run have a kt skip
+          /// </summary>
+    public bool DidKTSkip { get; set; }
 }
