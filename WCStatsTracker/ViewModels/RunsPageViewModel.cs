92a93426ef23e5fe41b3009e3939c149a3e39307
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-16 21:12:16 -0800

    Load from db instead of static init

--- WCStatsTracker/ViewModels/RunsPageViewModel.cs
+++ WCStatsTracker/ViewModels/RunsPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WCStatsTracker.Database;
 using WCStatsTracker.Models;
 
 namespace WCStatsTracker.ViewModels;
@@ -13,22 +14,10 @@ public partial class RunsPageViewModel : ViewModelBase
     [ObservableProperty]
     private List<WCRun> _runList;
 
+
     public RunsPageViewModel()
     {
-        ViewName = "Runs";
-        RunList = new List<WCRun>()
-            {
-                new WCRun{
-                    RunLength = new TimeSpan(1,20,30),
-                    CharactersFound = 5,
-                    EspersFound = 10,
-                    BossesKilled = 8,
-                    ChecksDone = 20,
-                    ChestsOpened = 50,
-                    DidKTSkip = false,
-                    DragonsKilled = 4
-                }
-        };
+        RunList = SqliteDataAccess.LoadWCRuns();
     }
 
 
