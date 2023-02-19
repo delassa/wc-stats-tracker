6cdb83810e6d3b0e43fcc10567bc6b19f910428d
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-18 23:30:21 -0800

    Add basic flag editing page

---
+++ WCStatsTracker/Views/FlagsPageView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace WCStatsTracker.Views;
+public partial class FlagsPageView : UserControl
+{
+    public FlagsPageView()
+    {
+        InitializeComponent();
+    }
+}
