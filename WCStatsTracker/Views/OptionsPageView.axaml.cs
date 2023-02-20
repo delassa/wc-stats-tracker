6f98dedbeb70b8302165842cd56587bab769455b
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-19 23:31:38 -0800

    Add blank view for an options page

---
+++ WCStatsTracker/Views/OptionsPageView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace WCStatsTracker.Views;
+public partial class OptionsPageView : UserControl
+{
+    public OptionsPageView()
+    {
+        InitializeComponent();
+    }
+}
