0194718f4f129a815dffd2c9ff87e8a41564ebbf
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-20 11:20:15 -0800

    Comments

--- WCStatsTracker/ViewModels/ViewModelBase.cs
+++ WCStatsTracker/ViewModels/ViewModelBase.cs
@@ -4,7 +4,14 @@ namespace WCStatsTracker.ViewModels
 {
     public class ViewModelBase : ObservableValidator
     {
+        /// <summary>
+        /// Name to display in menus for this view
+        /// </summary>
         public string? ViewName { get; set; }
+
+        /// <summary>
+        /// Name of the icon lookup in Material.Icon.Avalonia package
+        /// </summary>
         public string? IconName { get; set; }
     }
 }
