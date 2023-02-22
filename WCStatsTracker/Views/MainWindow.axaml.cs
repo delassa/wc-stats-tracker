3bb7c859b6ce01fdc954b9ccc6e2e11e939f4c73
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-22 15:02:54 -0800

    UI Work

--- WCStatsTracker/Views/MainWindow.axaml.cs
+++ WCStatsTracker/Views/MainWindow.axaml.cs
@@ -1,9 +1,12 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using WCStatsTracker.ViewModels;
 
 namespace WCStatsTracker.Views
 {
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
