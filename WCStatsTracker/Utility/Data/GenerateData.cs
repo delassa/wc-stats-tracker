0a28af32c3e49deeacb6236fa96fdf3b68abcb5c
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-23 01:32:42 -0800

    Code cleanup

--- WCStatsTracker/Utility/Data/GenerateData.cs
+++ WCStatsTracker/Utility/Data/GenerateData.cs
@@ -20,12 +20,11 @@ public static class GenerateData
     /// <param name="FlagSetCount">The number of flags to generate</param>
     /// <returns>A set of random flags filled out</returns>
     public static IEnumerable<FlagSet> GenerateFlags(int FlagSetCount) 
-    { 
-        var flags  = new List<FlagSet>();
-        var rand = new Random();
+    {
+        var flags = new List<FlagSet>();
         for (int i = 0; i < FlagSetCount; i++) 
         {
-            FlagSet flag = new FlagSet();
+            FlagSet flag = new();
             flag.Name += "Flag Set # " + i.ToString();
             flag.FlagString = "Flag String #" + i.ToString();
             flags.Add(flag);
@@ -44,15 +43,17 @@ public static class GenerateData
         var rand = new Random();
         for (var i = 0; i < Count; i++)
         {
-            WCRun run = new WCRun();
-            run.RunLength = new TimeSpan(rand.Next(0, 3), rand.Next(0, 60), rand.Next(0, 60));
-            run.CharactersFound = rand.Next(0, WC.Data.Characters.ConstantCount);
-            run.EspersFound = rand.Next(0, WC.Data.Espers.ConstantCount);
-            run.BossesKilled = rand.Next(0, WC.Data.Bosses.ConstantCount);
-            run.DragonsKilled = rand.Next(0, WC.Data.Dragons.ConstantCount);
-            run.ChecksDone = rand.Next(0, WC.Data.Checks.ConstantCount);
-            run.ChestsOpened = rand.Next(0, WC.Data.Chests.ConstantCount);
-            run.DidKTSkip = rand.Next(0, 1) != 0;
+            WCRun run = new()
+            {
+                RunLength = new TimeSpan(rand.Next(0, 3), rand.Next(0, 60), rand.Next(0, 60)),
+                CharactersFound = rand.Next(0, WC.Data.Characters.ConstantCount),
+                EspersFound = rand.Next(0, WC.Data.Espers.ConstantCount),
+                BossesKilled = rand.Next(0, WC.Data.Bosses.ConstantCount),
+                DragonsKilled = rand.Next(0, WC.Data.Dragons.ConstantCount),
+                ChecksDone = rand.Next(0, WC.Data.Checks.ConstantCount),
+                ChestsOpened = rand.Next(0, WC.Data.Chests.ConstantCount),
+                DidKTSkip = rand.Next(0, 1) != 0
+            };
             int len = 10;
             for (var j = 0; j<len; j++) 
             { 
