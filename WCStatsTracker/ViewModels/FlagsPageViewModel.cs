81b7d21d0af4fa4d40efc32069bc92fbc2c3e5d0
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-25 23:32:16 -0800

    Got the flags page almost functional

--- WCStatsTracker/ViewModels/FlagsPageViewModel.cs
+++ WCStatsTracker/ViewModels/FlagsPageViewModel.cs
@@ -1,40 +1,36 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
 using WCStatsTracker.Models;
 using WCStatsTracker.Services;
+using WCStatsTracker.Services.Messages;
 
 namespace WCStatsTracker.ViewModels;
+
 public partial class FlagsPageViewModel : ViewModelBase
 {
-
     private readonly IDatabaseService<FlagSet> _databaseService;
 
     [ObservableProperty]
-    private FlagSet? _workingFlagSet;
+    [NotifyCanExecuteChangedForAttribute(nameof(DeleteSelectedFlagCommand))]
+    private ObservableCollection<FlagSet>? _flagSetList;
 
     [ObservableProperty]
-    private string _flagNameText = string.Empty;
-
-    [ObservableProperty]
-    private string _flagStringText = string.Empty;
+    private FlagSet? _selectedFlagSet;
 
     [ObservableProperty]
     private int _selectedIndex;
 
     [ObservableProperty]
-    private ObservableCollection<FlagSet>? _flagSetList;
+    [NotifyCanExecuteChangedFor(nameof(SaveClickCommand))]
+    private FlagSet? _workingFlagSet;
 
     public FlagsPageViewModel()
     {
         _databaseService = new WCMockDatabaseService<FlagSet>();
         ViewName = "Flags";
         IconName = "Flag";
-        WorkingFlagSet = new FlagSet();
-
     }
 
     public FlagsPageViewModel(WCDBContextFactory wCDBContextFactory)
@@ -43,31 +39,79 @@ public partial class FlagsPageViewModel : ViewModelBase
         IconName = "Flag";
 
         _databaseService = new WCDatabaseService<FlagSet>(wCDBContextFactory);
-        _workingFlagSet = new FlagSet();
-        LoadData();
+        FlagSetList = new ObservableCollection<FlagSet>(_databaseService.GetAll());
+        WorkingFlagSet = new FlagSet();
     }
 
-
-    private async void LoadData()
+    partial void OnSelectedIndexChanged(int value)
     {
-        FlagSetList = new ObservableCollection<FlagSet>(await _databaseService.GetAll());
-    }
-
-    [RelayCommand]
-    private async Task SaveClick()
-    {
-        if (WorkingFlagSet is null) 
-        { 
-            throw new NullReferenceException($"Null reference of {typeof(FlagSet)} WorkingFlagSet"); 
-        }
-
-        FlagSet createdEntity = await _databaseService.Create(WorkingFlagSet);
+        DeleteSelectedFlagCommand.NotifyCanExecuteChanged();
         
-        if (FlagSetList is null)
-        {
-            throw new NullReferenceException($"Null reference of {typeof(ObservableCollection<FlagSet>)} FlagSetList");
-        }
+    }
 
-        FlagSetList.Add(createdEntity);
+    /// <summary>
+    /// Saves a new FlagSet entry into the database, also sends out a message
+    /// for other view models to monitor if they need to change their flag sets as well
+    /// </summary>
+    [RelayCommand(CanExecute = nameof(CanSaveClick))]
+    private void SaveClick()
+    {
+
+        _databaseService.Create(WorkingFlagSet);
+
+        WeakReferenceMessenger.Default.Send(new FlagSetAddMessage(WorkingFlagSet));
+
+        if (!FlagSetList.Contains(WorkingFlagSet))
+            FlagSetList.Add(WorkingFlagSet);
+        WorkingFlagSet = null;
+        WorkingFlagSet = new FlagSet();
+    }
+
+    /// <summary>
+    ///     Controls whether the save button for a new flag set can be clicked
+    /// </summary>
+    /// <returns></returns>
+    private bool CanSaveClick()
+    {
+        if (WorkingFlagSet == null ||
+            string.IsNullOrWhiteSpace(WorkingFlagSet.FlagString) ||
+            WorkingFlagSet.HasErrors ||
+            string.IsNullOrWhiteSpace(WorkingFlagSet.Name))
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+    /// <summary>
+    /// Deletes the selected flag and sends a message out about it so other
+    /// view models can update their collections
+    /// </summary>
+    [RelayCommand(CanExecute = nameof(CanDeleteSelectedFlag))]
+    private void DeleteSelectedFlag()
+    {
+        _databaseService.Delete(SelectedFlagSet);
+
+        WeakReferenceMessenger.Default.Send(new FlagSetDeleteMessage(SelectedFlagSet));
+
+        int previousIndex = SelectedIndex;
+        FlagSetList.Remove(SelectedFlagSet);
+        SelectedFlagSet = null;
+        SelectedFlagSet = new FlagSet();
+        DeleteSelectedFlagCommand.NotifyCanExecuteChanged();
+    }
+
+    /// <summary>
+    /// Controls whether the delete flag button is enabled
+    /// </summary>
+    /// <returns>True if its enabled, false otherwise</returns>
+    private bool CanDeleteSelectedFlag()
+    {
+        if (SelectedFlagSet is null)
+            return false;
+        if (FlagSetList.Count <= 0)
+            return false;
+        return true;
     }
 }
--- WCStatsTracker/Views/FlagsPageView.axaml.cs
+++ WCStatsTracker/Views/FlagsPageView.axaml.cs
@@ -1,10 +1,17 @@
 using Avalonia.Controls;
-
+using WCStatsTracker.ViewModels;
 namespace WCStatsTracker.Views;
+
 public partial class FlagsPageView : UserControl
 {
     public FlagsPageView()
     {
         InitializeComponent();
+    }
+
+    private void TextBoxTextChanging(object? sender, TextChangingEventArgs e)
+    {
+        var dc = DataContext as FlagsPageViewModel;
+        dc?.SaveClickCommand.NotifyCanExecuteChanged();
     }
 }
