4e3426e47ace18461fa546fe64433966c9bc20fa
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-25 23:28:03 -0800

    Messengingt system for notifying accross VM

---
+++ WCStatsTracker/Services/Messages/FlagSetAddMessage.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using WCStatsTracker.Models;
+
+namespace WCStatsTracker.Services.Messages;
+
+/// <summary>
+/// A message signifying that we need to update flag sets from the DB to synchronize them
+/// </summary>
+public sealed class FlagSetAddMessage : ValueChangedMessage<FlagSet>
+{
+    public FlagSetAddMessage(FlagSet flagSet) : base(flagSet)
+    { }
+}
---
+++ WCStatsTracker/Services/Messages/FlagSetDeleteMessage.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using WCStatsTracker.Models;
+
+namespace WCStatsTracker.Services.Messages;
+
+public sealed class FlagSetDeleteMessage : ValueChangedMessage<FlagSet>
+{
+    public FlagSetDeleteMessage(FlagSet flagSet) : base(flagSet)
+    { }
+}
