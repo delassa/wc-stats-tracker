1045f4131865013c13b1673a543232b03cd88c7a
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-02-25 23:30:15 -0800

    Some code cleanup

--- WCStatsTracker/Utility/Data/GenerateData.cs
+++ WCStatsTracker/Utility/Data/GenerateData.cs
@@ -1,39 +1,35 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using WCStatsTracker.Models;
-
+using WCStatsTracker.WC.Data;
 namespace WCStatsTracker.Utility.Data;
 
-
 /// <summary>
-/// Static class to generate some random data
+///     Static class to generate some random data
 /// </summary>
 public static class GenerateData
 {
-
     /// <summary>
-    /// Generates a fake set of flags
+    ///     Generates a fake set of flags
     /// </summary>
     /// <param name="FlagSetCount">The number of flags to generate</param>
     /// <returns>A set of random flags filled out</returns>
-    public static IEnumerable<FlagSet> GenerateFlags(int FlagSetCount) 
+    public static IEnumerable<FlagSet> GenerateFlags(int FlagSetCount)
     {
         var flags = new List<FlagSet>();
-        for (int i = 0; i < FlagSetCount; i++) 
+        for (var i = 0; i < FlagSetCount; i++)
         {
             FlagSet flag = new();
-            flag.Name += "Flag Set # " + i.ToString();
-            flag.FlagString = "Flag String #" + i.ToString();
+            flag.Name += "Flag Set # " + i;
+            flag.FlagString = "Flag String #" + i;
             flags.Add(flag);
         }
+
         return flags;
     }
-    
+
     /// <summary>
-    /// Returns a set of random runs
+    ///     Returns a set of random runs
     /// </summary>
     /// <param name="Count">The number of runs to generate</param>
     /// <returns>A collection of the runs</returns>
@@ -46,22 +42,19 @@ public static class GenerateData
             WCRun run = new()
             {
                 RunLength = new TimeSpan(rand.Next(0, 3), rand.Next(0, 60), rand.Next(0, 60)),
-                CharactersFound = rand.Next(0, WC.Data.Characters.ConstantCount),
-                EspersFound = rand.Next(0, WC.Data.Espers.ConstantCount),
-                BossesKilled = rand.Next(0, WC.Data.Bosses.ConstantCount),
-                DragonsKilled = rand.Next(0, WC.Data.Dragons.ConstantCount),
-                ChecksDone = rand.Next(0, WC.Data.Checks.ConstantCount),
-                ChestsOpened = rand.Next(0, WC.Data.Chests.ConstantCount),
+                CharactersFound = rand.Next(0, Characters.ConstantCount),
+                EspersFound = rand.Next(0, Espers.ConstantCount),
+                BossesKilled = rand.Next(0, Bosses.ConstantCount),
+                DragonsKilled = rand.Next(0, Dragons.ConstantCount),
+                ChecksDone = rand.Next(0, Checks.ConstantCount),
+                ChestsOpened = rand.Next(0, Chests.ConstantCount),
                 DidKTSkip = rand.Next(0, 1) != 0
             };
-            int len = 10;
-            for (var j = 0; j<len; j++) 
-            { 
-                run.Seed += ((char)rand.Next(1, 26) + 64).ToString().ToLower(); 
-            }
+            var len = 10;
+            for (var j = 0; j < len; j++) run.Seed += ((char)rand.Next(1, 26) + 64).ToString().ToLower();
             runs.Add(run);
         }
-        return runs;        
-    }
 
+        return runs;
+    }
 }
