169c054d9788916a65c01defeda5f10a762675c8
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-03-01 20:25:50 -0800

    Add in message for a run being saved in the database

---
+++ WCStatsTracker/Services/Messages/RunSavedMessage.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCStatsTracker.Models;
+
+namespace WCStatsTracker.Services.Messages;
+
+/// <summary>
+/// A message signifying that we need to update flag sets from the DB to synchronize them
+/// </summary>
+public sealed class RunSavedMessage : ValueChangedMessage<WCRun>
+{
+    public RunSavedMessage(WCRun savedRun) : base(savedRun)
+    { }
+}
