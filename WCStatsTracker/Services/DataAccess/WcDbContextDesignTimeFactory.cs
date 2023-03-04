1c929f30ce3b73df4983c3f9342cf8f94b64f059
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-03-04 09:07:25 -0800

    Add in design time factory for migrations support

---
+++ WCStatsTracker/Services/DataAccess/WcDbContextDesignTimeFactory.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace WCStatsTracker.Services.DataAccess;
+
+public class WcDbContextDesignTimeFactory : IDesignTimeDbContextFactory<WcDbContext>
+{
+    public WcDbContext CreateDbContext(string [] args = null)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<WcDbContext>();
+        var fixedConnectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString
+            .Replace("{AppDir}", AppDomain.CurrentDomain.BaseDirectory);
+        optionsBuilder.UseSqlite(fixedConnectionString);
+
+        return new WcDbContext(optionsBuilder.Options);
+    }
+}
