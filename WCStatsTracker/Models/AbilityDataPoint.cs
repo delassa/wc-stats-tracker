85e93b637c18c2930534851808512105f8e08de8
Lucas Clerget (Delassa) <lpclerget@gmail.com>
2023-03-11 08:12:34 -0800

    Rename and add a timespan for each point in the charts

--- WCStatsTracker/Models/AbilityDataPoint.cs
+++ WCStatsTracker/Models/AbilityDataPoint.cs
@@ -1,10 +1,12 @@
+using System;
 namespace WCStatsTracker.Models;
 
 /// <summary>
 /// Simply class to hold the count of a certain ability
 /// </summary>
-public class AbilityCount
+public class AbilityDataPoint
 {
     public string Name { get; set; } = string.Empty;
     public int Count { get; set; } = 0;
+    public TimeSpan AverageRunLength = TimeSpan.Zero;
 }
--- WCStatsTracker/Models/CharacterDataPoint.cs
+++ WCStatsTracker/Models/CharacterDataPoint.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Dynamic;
 namespace WCStatsTracker.Models;
 
 /// <summary>
 /// Simple class to hold a count of a certain character
 /// </summary>
-public class CharacterCount
+public class CharacterDataPoint
 {
     public string Name { get; set; } = string.Empty;
     public int Count { get; set; } = 0;
+    public TimeSpan AverageRunLength { get; set; } = TimeSpan.Zero;
 }
